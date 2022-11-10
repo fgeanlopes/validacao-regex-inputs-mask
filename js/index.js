0d2ca70fdd3dd3541b55abc5c68afdb8971b13e0
fgeanlopes <f.geanlopes@gmail.com>
2022-11-09 23:24:01 -0300

    add mask cnpj and phone

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const mask = {
     cpf(value){
+        // CPF: 000.000.000-00
         return value
         .replace(/\D/g, '')
         .replace(/(\d{3})(\d{1})/, '$1.$2')
@@ -9,10 +10,24 @@ const mask = {
     },
     
     cnpj(value){
+        // CNPJ: 00.000.000/0000-00
         return value
+        .replace(/\D/g, '')
+        .replace(/(\d{2})(\d{1})/, '$1.$2')
+        .replace(/(\d{3})(\d{1})/, '$1.$2')
+        .replace(/(\d{3})(\d{1})/, '$1/$2')
+        .replace(/(\d{4})(\d{1,2})/, '$1-$2')
+        .replace(/(-\d{2})\d+?$/, '$1')
     },
     telefone(value){
+        // TELEFONE:(00)  0000-0000
+        // CELULAR: (00) 00000-0000
         return value
+        .replace(/\D/g, '')
+        .replace(/(\d{2})(\d{1})/, '($1) $2')
+        .replace(/(\d{4})(\d{1})/, '$1-$2')
+        .replace(/(\d{4})-(\d{1})(\d{4})/, '$1$2-$3')
+        .replace(/(-\d{4,})\d+?$/, '$1')
     },
     cep(value){
         return value
